5ab7276d6842e79ed2e32733af9a8834bcc5bec3
tjadhav <tjadhav@qualys.com>
2022-06-28 14:36:32 +0530

    code changes

---
+++ PowershellScript.bash
@@ -0,0 +1,5 @@
+#!/usr/bin/env bash
+Write-Host "Powershell Script"
+$str1="Powershell "
+$str2="Script Output"
+$str1+$str2
---
+++ ShellScript.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+#Declare first string
+string1="Shell"
+#Declare second string
+string2=" Script Output"
+#Combine first and second string
+string3=$string1$string2
+# Print the third string by combining with other string
+echo "$string3 printed!!"
